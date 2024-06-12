5a9eb42b9ee1703e53dc63b482e75521142c0a19
roha-rizwan <roharizwan7@mail.com>
2024-06-12 11:31:21 +1000

    API test

--- src/test/java/stepDefinitions/APISteps.java
+++ src/test/java/stepDefinitions/APISteps.java
@@ -1,11 +1,15 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
+
+import java.util.List;
 
 public class APISteps {
 
@@ -18,11 +22,50 @@ public class APISteps {
          httpRequest = RestAssured.given();
 
         response = httpRequest.get("");
-        System.out.println("testings");
+        response.prettyPrint();
     }
 
     @Then("I validate a successful response")
     public void iValidateASuccessfulResponse() {
         Assert.assertEquals("Unsuccessful response code - "+response.getStatusCode(),response.getStatusCode(),200);
     }
-}
+
+    @And("Then i validate the body")
+    public void thenIValidateTheBody() {
+        String responseBody= response.asString();
+
+        Assert.assertTrue("Response body contains Norway ", responseBody.contains("Lasalle"));
+    }
+
+
+    @Then("Then i validate email detail is not null")
+    public void thenIValidateemailDetailsIsNotNull() {
+        JsonPath jsonPathEvaluator=response.jsonPath();
+
+        List<String> emails = jsonPathEvaluator.getList("results.email");
+
+        for(String email : emails)
+        {
+            System.out.println("Login details: " + email);
+            Assert.assertFalse("Email address is null", email.isEmpty());
+
+        }
+
+    }
+
+    @When("I issue an invalid GET request")
+    public void iIssueAnInvalidGETRequest() {
+        RestAssured.baseURI = "https://randomuser.me/apis/";
+        httpRequest = RestAssured.given();
+
+        response = httpRequest.get("");
+        response.prettyPrint();
+    }
+
+    @Then("I validate a unsuccessful response")
+    public void iValidateAUnsuccessfulResponse() {
+        Assert.assertEquals("Unsuccessful response code - "+response.getStatusCode(),response.getStatusCode(),404);
+    }
+    }
+
+
--- src/test/resources/functionalTests/APItests.feature
+++ src/test/resources/functionalTests/APItests.feature
@@ -3,3 +3,11 @@ Feature: Sample API tests
   Scenario: To test GET request
     When I issue a GET request
     Then I validate a successful response
+
+  Scenario: To test GET request
+    When I issue an invalid GET request
+    Then I validate a unsuccessful response
+
+  Scenario: To test email detail is not null
+    When I issue a GET request
+    Then Then i validate email detail is not null
